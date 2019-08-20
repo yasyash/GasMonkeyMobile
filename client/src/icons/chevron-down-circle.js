477191f7df619dbdf26aa2bc7d67eadaf68e0fa5
Yaroslav Shkliar <mail@ilit.ru>
2019-08-20 16:56:06 +0300

    FTP package change from ftp to basic-ftp. FTP handling and errors catching changed. FTP errors logging added. Some icons changed into new ones.

---
+++ client/src/icons/chevron-down-circle.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _createSvgIcon = _interopRequireDefault(require("@material-ui/icons/utils/createSvgIcon"));
+
+var _default = (0, _createSvgIcon.default)(_react.default.createElement(_react.default.Fragment, null, 
+     _react.default.createElement("path", {
+  d: "M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M6,10L12,16L18,10L16.6,8.6L12,13.2L7.4,8.6L6,10Z"
+})), 'ZoomOutOutlined');
+
+exports.default = _default;
