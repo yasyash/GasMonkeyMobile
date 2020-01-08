1846bc4ed0b0ff67cd03b0ff7e25451a2033206b
Yaroslav Shkliar <mail@ilit.ru>
2020-01-08 22:34:06 +0300

    Type of license changed. Version was upgraded...

--- client/src/MainApp.js
+++ client/src/MainApp.js
@@ -21,7 +21,7 @@ class MainApp extends Component {
                 <div className = "form-control">
                     <footer className="App-footer"></footer>
                     <p className="App App-intro">
-                        Developed by ILIT.RU, 2017-2019. (version 1.02)
+                        Developed by ILIT.RU, 2017-2020. (version 1.03)
                               </p>
                 </div>
             </div >
